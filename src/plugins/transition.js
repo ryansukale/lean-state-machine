4b212ee8adf7c7eb3dbca74793bcae17b5c78306
Ryan Sukale <ryansukale@gmail.com>
2020-04-27 19:47:57 -0400

    Move transition file to plugins folder

--- src/plugins/transition.js
+++ src/plugins/transition.js
@@ -1,39 +1,39 @@
-import createMachine from './createMachine';
-import toArray from './lib/toArray';
+import createMachine from "./createMachine";
+import toArray from "./lib/toArray";
 
 function createTransitionMachine(params, options) {
   const machine = createMachine(params, options);
-  
+
   const update = (nextState, updater) => {
-    if(!machine.isValidState(nextState)) {
+    if (!machine.isValidState(nextState)) {
       return false;
     }
-    
+
     const state = machine.getState();
     const currentOn = params.states[state].on;
     const nextOn = params.states[nextState].on;
     let event;
 
     if (currentOn || nextOn) {
-      event = { transition : [state, nextState] };
+      event = { transition: [state, nextState] };
     }
 
     if (currentOn && currentOn.exit) {
       const fns = toArray(currentOn.exit);
-      fns.forEach(f => f(machine.getContext(), event));
+      fns.forEach((f) => f(machine.getContext(), event));
     }
 
     const context = machine.update(nextState, updater);
 
     if (nextOn && nextOn.entry) {
       const fns = toArray(nextOn.entry);
-      fns.forEach(f => f(context, event));
+      fns.forEach((f) => f(context, event));
     }
 
     return context;
-  }
+  };
 
-  return {...machine, update};
+  return { ...machine, update };
 }
 
 export default createTransitionMachine;
