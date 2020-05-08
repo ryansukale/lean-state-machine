887328a35fa1891e4d63bf724a89cf786b0d26b4
Ryan Sukale <ryansukale@gmail.com>
2020-05-08 17:14:14 -0400

    Add a simple test

--- tests/createMachine.test.js
+++ tests/createMachine.test.js
@@ -1,6 +1,7 @@
-import createMachine from '../createMachine';
+import createMachine from '../src/createMachine';
+import { expect } from 'chai';
 
-let machine = createMachine({
+const params = {
   context: {result: 10, error: undefined},
   initial: 'init',
   states: {
@@ -9,22 +10,36 @@ let machine = createMachine({
     success: {},
     error: {},
   }
-}, {
+};
+
+const options = {
   onUpdate: [
     (ctx, event) => console.log('onUpdate event', event)
   ]
+};
+
+describe('createMachine', function () {
+  it('creates a machine with an initial state', function () {
+    const machine = createMachine(params, options);
+    expect(machine.getState()).to.eql({
+      value: 'init',
+      context: {result: 10, error: undefined}
+    });
+  });
 });
 
-console.clear();
-machine.update(machine.states.loading);
-debug(machine);
-console.log('')
-machine.update(machine.states.error, ctx => ({error: 'Error! 401'}));
-debug(machine);
-console.log('')
-machine.update(machine.states.loading, ctx => ({error: 'Error! 401'}));
-debug(machine);
-console.log('')
-machine.update(machine.states.success, ctx => ({result: 200}));
-debug(machine);
-console.log('')
+// let machine = createMachine(params, options);
+
+// console.clear();
+// machine.update(machine.states.loading);
+// debug(machine);
+// console.log('')
+// machine.update(machine.states.error, ctx => ({error: 'Error! 401'}));
+// debug(machine);
+// console.log('')
+// machine.update(machine.states.loading, ctx => ({error: 'Error! 401'}));
+// debug(machine);
+// console.log('')
+// machine.update(machine.states.success, ctx => ({result: 200}));
+// debug(machine);
+// console.log('')
