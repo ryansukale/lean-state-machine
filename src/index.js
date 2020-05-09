daaa2bc4a3f2af136bb608b108c481b3b56bfc52
Ryan Sukale <ryansukale@gmail.com>
2020-05-09 19:04:22 -0400

    Add some release scripts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import createMachine from "./createMachine";
 import createExposedMachine from "./createExposedMachine";
 import enhance from "./enhance";
+import expose from "./plugins/expose";
+const plugins = { expose };
 
-export { enhance, createMachine, createExposedMachine };
+export { enhance, createMachine, createExposedMachine, plugins };
